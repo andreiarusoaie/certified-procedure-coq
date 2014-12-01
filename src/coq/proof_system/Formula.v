d638a8064d12cefaa6ddb6fa5de35e6bbc98e4b2
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2014-12-01 15:37:08 +0100

    Commit for debugging purposes: module imports

--- src/coq/proof_system/Formula.v
+++ src/coq/proof_system/Formula.v
@@ -1,25 +1,33 @@
 Require Import ZArith.
+Require Import FMapPositive.
 
-(* These are language dependent *)
-Parameter Term : Set .
-Parameter Id : Set .
 
-(* Variables - I'm not sure this is the best solution now *)
-Inductive Var : Set := varF : Z -> Var .
+Module Type TERM .
+  Parameter Term : Type .
+End TERM. 
 
-(* Matching logic formulas *)
-Inductive Formula : Set :=
-  | basic_pattern : Term -> Formula
-  | andF : Formula -> Formula -> Formula
-  | orF : Formula -> Formula -> Formula
-  | notF : Formula -> Formula
-  | impliesF : Formula -> Formula -> Formula 
-  | existsF : Var -> Formula -> Formula
-.
 
-(* Rules = Reachability Logic formulas *)
-Inductive Rule : Set := rule : Formula -> Formula -> Rule .
-Notation "L => R" := (rule L R) (at level 100).
+Module MLFormula (M : TERM).
+  Import M.
+  
+  (* Variables - ??? *)
+  Inductive Var : Set := varF : Z -> Var .
 
-(* Reachability rules system *)
-Definition System := Rule -> Prop .
+  (* Matching logic formulas *)
+  Inductive Formula : Type :=
+    | bp : Term -> Formula
+    | T : Formula 
+    | andF : Formula -> Formula -> Formula
+    | orF : Formula -> Formula -> Formula
+    | notF : Formula -> Formula
+    | impliesF : Formula -> Formula -> Formula 
+    | existsF : Var -> Formula -> Formula
+  .
+
+  (* Rules = Reachability Logic formulas *)
+  Inductive Rule : Type := rule : Formula -> Formula -> Rule .
+  Notation "L => R" := (rule L R) (at level 100).
+  
+  (* Reachability rules system *)
+  Definition System := Rule -> Prop .
+End MLFormula.
--- src/coq/proof_system/ProofSystem.v
+++ src/coq/proof_system/ProofSystem.v
@@ -1,26 +1,31 @@
 Require Import List.
 Require Import Formula.
-Require Import Model.
 
-Fixpoint delta_S (phi : Formula) (S : System) : Formula := phi .
-Fixpoint delta_G (phi : Formula) (C : Rule) : Formula := phi .
-Fixpoint covers (r : Rule) (phi : Formula) := True .
-Fixpoint derivable (phi : Formula) (S : System) := True .
+Module ProofSystem .
+  Import Formula .
 
-Inductive PS : System -> System -> Formula -> Formula -> Prop :=
-| ps_ss : forall (S : System) (G : System)
-                 (phi : Formula) (phi' : Formula),
-            derivable phi S -> PS S G phi (delta_S phi S)
-| ps_circ : forall (S : System) (G : System)
-                   (phi : Formula) (phi' : Formula) (phi'' : Formula),
-              G (phi' => phi'') -> covers (phi' => phi'') phi ->
-              PS S G phi (delta_G phi (phi' => phi''))
-| ps_impl : forall (S : System) (G : System)
-                   (phi : Formula) (phi' : Formula),
+  (* TODO: implement the functions below *)
+  Fixpoint delta_S (phi : Formula) (S : System) : Formula := phi .
+  Fixpoint delta_G (phi : Formula) (C : Rule) : Formula := phi .
+  Fixpoint covers (r : Rule) (phi : Formula) := True .
+  Fixpoint derivable (phi : Formula) (S : System) := True .
+
+  Inductive PS : System -> System -> Formula -> Formula -> Prop :=
+    | ps_ss : forall (S : System) (G : System)
+                     (phi : Formula) (phi' : Formula),
+                derivable phi S -> PS S G phi (delta_S phi S)
+    | ps_circ : forall (S : System) (G : System)
+                       (phi : Formula) (phi' : Formula) (phi'' : Formula),
+                  G (phi' => phi'') -> covers (phi' => phi'') phi ->
+                  PS S G phi (delta_G phi (phi' => phi''))
+    | ps_impl : forall (S : System) (G : System)
+                       (phi : Formula) (phi' : Formula),
             Valid (impliesF phi phi') -> PS S G phi phi'
-| ps_ca : forall (S : System) (G : System)
-                 (phi : Formula) (phi' : Formula) (phi'' : Formula),
-               PS S G phi' phi -> PS S G phi'' phi -> PS S G (orF phi' phi'') phi
-| ps_trans : forall (S : System) (G : System)
-                 (phi : Formula) (phi' : Formula) (phi'' : Formula),
-               PS S G phi phi'' -> PS S G phi'' phi' -> PS S G phi phi' .
+    | ps_ca : forall (S : System) (G : System)
+                     (phi : Formula) (phi' : Formula) (phi'' : Formula),
+                PS S G phi' phi -> PS S G phi'' phi -> PS S G (orF phi' phi'') phi
+    | ps_trans : forall (S : System) (G : System)
+                        (phi : Formula) (phi' : Formula) (phi'' : Formula),
+                   PS S G phi phi'' -> PS S G phi'' phi' -> PS S G phi phi' .
+  
+End ProofSystem.
