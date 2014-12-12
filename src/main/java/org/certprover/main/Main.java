d55d64c87b90e1995f73f0d36a9a7f0cfcc9c5b2
andreiarusoaie <andrei.arusoaie@gmail.com>
2014-12-12 14:47:12 +0200

    Added the skeleton of the gradle project and a working hello world app.

---
+++ src/main/java/org/certprover/main/Main.java
@@ -0,0 +1,12 @@
+package org.certprover.main;
+
+/**
+ * Created by Andrei on 12/12/2014.
+ *
+ * The main class of the tool which gets the arguments and triggers the verification.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
