02ca2186724e32f5fb9e2b330eedfcbfd5259667
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2016-06-02 11:07:03 +0300

    Added model stuff.

--- src/coq/lang.v
+++ src/coq/lang.v
@@ -9,19 +9,36 @@ Module Lang <: Formulas.
   (* syntax *)
   Inductive Exp : Type := 
   | exp_var : Var -> Exp
+  | id : string -> Exp
   | val : nat -> Exp
   | plus : Exp -> Exp -> Exp.
 
   Inductive Stmt : Type :=
-  | assign : Var -> Exp -> Stmt
-  | seq : Stmt -> Stmt -> Stmt
+  | assign : string -> Exp -> Stmt
+  | seq : list Stmt -> Stmt
   | stmt_var : Var -> Stmt.
 
   (* configuration *)
-  Inductive MapItem := item : Var -> Exp -> MapItem.
-  Definition Cfg := (Stmt, list MapItem).
+  Inductive MapItem := item : string -> Exp -> MapItem.
+  Inductive Cfg := 
+  | cfg : Stmt -> list MapItem -> Cfg
+  | cfg_var : Var -> Cfg.
 
   
+
+  (* model and state *)
+  Inductive _stmt : Type := 
+    | _assign : string -> nat -> _stmt
+    | _seq : list _stmt -> _stmt.
+   
+  Definition _map_item := (string * nat)%type.
+  Definition State : Type := (_stmt * _map_item)%type. 
+  Inductive Model : Type := 
+    | to_m_nat : nat -> Model
+    | to_m_stmt : _stmt -> Model
+    | to_m_map_item : _map_item -> Model
+    | to_m_state : State -> Model.
+
 
 
   (* var equality *)
@@ -86,11 +103,16 @@ Module Lang <: Formulas.
       rewrite He, He'; trivial.
   Qed.
 
+  Lemma var_eq_refl : 
+    forall x, var_eq x x = true.
+  Proof.
+    intros x; apply var_eq_true; trivial.
+  Qed.
 
-        
-        
-        
-      
+  
+
+
+End Lang.  
 
       
                        
