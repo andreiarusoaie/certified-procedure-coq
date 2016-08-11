c983338a931b82c9a4ef09f8518c77117a49fb4e
andrei.arusoaie <andrei.arusoaie@gmail.com>
2016-08-11 06:01:13 -0700

    New language. Another trial.

--- src/coq/lang.v
+++ src/coq/lang.v
@@ -2,76 +2,143 @@ Require Import ml.
 Require Import String.
 Require Import List.
 Require Import Classical.
+Require Import ZArith.
 
 Module Lang <: Formulas.
 
-(*  Inductive Var : Type := var : string -> string -> Var. *)
   Open Scope string_scope.
 
   (* syntax *)
-  Inductive ExpVar : Type := exp_v : string -> ExpVar.
+  (*
+     Exp ::= Id | Int
+           | Exp + Exp
+
+     BExp ::= Bool 
+            | Exp <= Exp
+            | ! Exp
+
+     Stmt ::= 
+            Id = Exp;
+            if BExp then Stmt else Stmt
+            while BExp do Stmt
+            Stmt Stmt
+   *)
+
+  Inductive ID : Type :=
+  | a : ID
+  | b : ID
+  | c : ID
+  | n : ID
+  | i : ID
+  | x : ID
+  | y : ID
+  | s : ID.
+
+  Inductive Var : Type :=
+  | A : Var
+  | B : Var
+  | C : Var
+  | N : Var
+  | I : Var
+  | X : Var
+  | Y : Var
+  | S : Var.
+          
+  
   Inductive Exp : Type := 
-  | exp_var : ExpVar -> Exp
-  | id : string -> Exp
-  | val : nat -> Exp
+  | id : ID -> Exp
+  | val : Z -> Exp
+  | var_exp : Var -> Exp
   | plus : Exp -> Exp -> Exp.
   
 
-  Eval compute in (exp_var (exp_v "a")).
-  Eval compute in (plus (id "a") (exp_var (exp_v "a"))).
-  Eval compute in (plus (id "a") (val 4)).
+  Notation "$ A" := (id A) (at level 100).
+  Notation "! A" := (var_exp A) (at level 100).
+  Eval compute in ($ a).
+  Eval compute in (val 2).
+  Eval compute in (! A).
+  Eval compute in (plus (! A) ($ a)).  
 
-  Inductive StmtVar : Type := stmt_v : string -> StmtVar.
+
+  Inductive BExp : Type :=
+  | TT : BExp
+  | FF : BExp
+  | var_bexp : Var -> BExp
+  | leq : Exp -> Exp -> BExp .
+
+  Eval compute in TT.
+  Eval compute in (val 4).
+  Eval compute in (leq (! A) (val 3)).
+  
+
   Inductive Stmt : Type :=
-  | stmt_var : StmtVar -> Stmt 
-  | assign : string -> Exp -> Stmt
-  | seq : list Stmt -> Stmt.
+  | assign : ID -> Exp -> Stmt
+  | var_stmt : Var -> Stmt
+  | ifelse : BExp -> Stmt -> Stmt -> Stmt
+  | while : BExp -> Stmt -> Stmt
+  | skip : Stmt
+  | seq : Stmt -> Stmt -> Stmt .
 
-  Eval compute in (assign "a" (val 10)).
-  Eval compute in (assign "a" (plus (id "a") (val 10))).
-  Eval compute in 
-      assign "a" (plus (id "a") (val 10)) :: (assign "a" (val 10)) :: nil.
+
+  Notation "A <- B " := (assign A B) (at level 100).
+  Notation "A ; B" := (seq A B) (at level 100).
+  
+  Eval compute in  (i <- (val 0)).
+  Eval compute in (var_stmt S).
+  Eval compute in (ifelse (leq ($ i) ($ n))  (assign s (plus ($ s) ($ i))) skip) .
+  Eval compute in (i <- (val 0) ; (s <- (val 0))).
+  Eval compute in (i <- (val 0) ; (s <- (val 0)) ;
+                   (while (leq ($ i) ($ n))
+                          ((s <- (plus ($ s) ($ i))) ;
+                           (i <- (plus ($ i) (val 1))))
+                   )).
+  
 
   (* configuration *)
-  Inductive MapItemVar : Type := mi_v : string -> MapItemVar.
   Inductive MapItem := 
-  | mapitem_var : MapItemVar -> MapItem
-  | item : string -> Exp -> MapItem.
+  | var_mi : Var -> MapItem
+  | item : ID -> Exp -> MapItem.
 
-  Inductive CfgVar : Type := 
-  | cfg_v : string -> CfgVar
-  | z : CfgVar .
   Inductive Cfg := 
-  | cfg : Stmt -> list MapItem -> Cfg
-  | cfg_var : CfgVar -> Cfg.
+  | var_cfg : Var -> Cfg
+  | cfg : Stmt -> list MapItem -> Cfg .
 
+(*  Notation "A |-> B" := (item_z A B) (at level 100). *)
   Notation "A |-> B" := (item A B) (at level 100).
   Eval compute in (cfg 
-                     (assign "a" (plus (id "a") (val 10))) 
-                     (("a" |-> (exp_var (exp_v "A"))) :: nil)).
 
-  (* Var *)
-  Inductive Var : Type :=
-  | var_exp : ExpVar -> Var
-  | var_stmt : StmtVar -> Var
-  | var_mapitem : MapItemVar -> Var 
-  | var_cfg : CfgVar -> Var.
+                     (i <- (val 0) ; (s <- (val 0)) ;
+                   (while (leq ($ i) ($ n))
+                          ((s <- (plus ($ s) ($ i))) ;
+                           (i <- (plus ($ i) (val 1))))
+                   ))
+                     
+                   ((n |-> (! N)) :: nil)).
 
   
   (* model and state: equiv classes? *)
   Inductive _exp : Type :=
-    | _nat : nat -> _exp
+    | _int : Z -> _exp
     | _plus : _exp -> _exp -> _exp
-    | _id : string -> _exp.
+    | _id : ID -> _exp. 
+
+  Inductive _bexp : Type :=
+  | _bool : bool -> _bexp
+  | _leq : _exp -> _exp -> _bexp.
+                             
   
-  Inductive _stmt : Type := 
-    | _assign : string -> _exp -> _stmt
-    | _seq : list _stmt -> _stmt.
+  Inductive _stmt : Type :=
+  | _skip : _stmt
+  | _assign : ID -> _exp -> _stmt
+  | _ifelse : _bexp -> _stmt -> _stmt -> _stmt 
+  | _while : _bexp -> _stmt -> _stmt 
+  | _seq : _stmt -> _stmt -> _stmt.
    
-  Definition _map_item := (string * nat)%type.
-  Definition State : Type := (_stmt * _map_item)%type. 
+  Definition _map_item := (ID * _exp)%type.
+  Definition State : Type := (_stmt * list _map_item)%type.
   Inductive Model : Type := 
-    | to_m_nat : nat -> Model
+    | to_m_exp : _exp -> Model
+    | to_m_bexp : _bexp -> Model
     | to_m_stmt : _stmt -> Model
     | to_m_map_item : _map_item -> Model
     | to_m_state : State -> Model.
@@ -79,32 +146,16 @@ Module Lang <: Formulas.
 
 
   (* var equality *)
-  Definition var_eq_exp (X Y : ExpVar) : bool := 
-    match X, Y with 
-      | exp_v x, exp_v y => if string_dec x y then true else false
-    end.
-  Definition var_eq_stmt (X Y : StmtVar) : bool := 
-    match X, Y with 
-      | stmt_v x, stmt_v y => if string_dec x y then true else false
-    end.
-  Definition var_eq_mapitem (X Y : MapItemVar) : bool := 
-    match X, Y with 
-      | mi_v x, mi_v y => if string_dec x y then true else false
-    end.
-  Definition var_eq_cfg (X Y : CfgVar) : bool := 
-    match X, Y with 
-      | cfg_v x, cfg_v y => if string_dec x y then true else false
-      | z, z => true
-      | _, _ => false
-    end.
-
-
-  Definition var_eq (X Y : Var) : bool :=
+  Definition var_eq (X Y : Var) : bool := 
     match X, Y with
-      | var_exp x, var_exp y => var_eq_exp x y
-      | var_stmt x, var_stmt y => var_eq_stmt x y
-      | var_mapitem x, var_mapitem y => var_eq_mapitem x y
-      | var_cfg x, var_cfg y => var_eq_cfg x y
+      | A, A => true
+      | B, B => true
+      | C, C => true
+      | N, N => true
+      | I, I => true
+      | X, X => true
+      | Y, Y => true
+      | S, S => true
       | _, _ => false
     end.
 
@@ -113,65 +164,11 @@ Module Lang <: Formulas.
   Proof.
     intros a b.
     split; intros H.
-    - case_eq a; intros v Hv; case_eq b; intros v' Hv'; unfold var_eq in H; subst; try inversion H.
-      + unfold var_eq_exp in H.
-        case_eq v; intros v0 Hv0; subst.
-        case_eq v'; intros v0' Hv0'; subst.
-        case_eq (string_dec v0 v0'); intros; subst; trivial.
-        rewrite H0 in H.
-        inversion H.
-      + unfold var_eq_stmt in H.
-        case_eq v; intros v0 Hv0; subst.
-        case_eq v'; intros v0' Hv0'; subst.
-        case_eq (string_dec v0 v0'); intros; subst; trivial.
-        rewrite H0 in H.
-        inversion H.
-      + unfold var_eq_mapitem in H.
-        case_eq v; intros v0 Hv0; subst.
-        case_eq v'; intros v0' Hv0'; subst.
-        case_eq (string_dec v0 v0'); intros; subst; trivial.
-        rewrite H0 in H.
-        inversion H.
-      + unfold var_eq_cfg in H.
-        case_eq v; intros c.
-        * intros Hc.
-          subst.
-          case_eq v'; intros c'.
-          intros Hc'.
-          subst.
-          case_eq (string_dec c c'); intros.
-          subst. trivial.
-          rewrite H0 in H.
-          inversion H.
-          case_eq v'; intros c0.
-          subst.
-          inversion H.
-          subst.
-          inversion H.
-        * subst. 
-          case_eq v'.
-          intros s Hs.
-          subst.
-          inversion H.
-          intros Hs'.
-          subst.
-          trivial.
-    - subst.
-      unfold var_eq.
-      case_eq b; intros e He.
-      + unfold var_eq_exp; case_eq e; intros s Hs; case_eq (string_dec s s); intros; trivial.
-        contradiction n; trivial.
-      + unfold var_eq_stmt; case_eq e; intros s Hs; case_eq (string_dec s s); intros; trivial.
-        contradiction n; trivial.
-      + unfold var_eq_mapitem; case_eq e; intros s Hs; case_eq (string_dec s s); intros; trivial.
-        contradiction n; trivial.
-      + unfold var_eq_cfg; case_eq e.
-        * intros s Hs. case_eq (string_dec s s); intros; trivial.
-          contradiction n; trivial.
-        * intros Hs.
-          subst.
-          trivial.
+    - case_eq a; intros Ha; case_eq b; intros Hb; rewrite Ha, Hb in H; simpl in H;
+      try inversion H; trivial.
+    - rewrite H; case_eq b; intros Hb; simpl; trivial.
   Qed.
+
 
   Lemma var_eq_false:
     forall a b, var_eq a b = false <-> a <> b.
@@ -183,12 +180,8 @@ Module Lang <: Formulas.
       apply var_eq_true in H'.
       rewrite H in H'.
       inversion H'.
-    - assert (H' : var_eq a b = false \/ ~ var_eq a b = false ).
-      apply classic.
-      destruct H' as [H' | H']; trivial.
-      case_eq (var_eq a b); intros Hv; trivial.
-      rewrite var_eq_true in Hv.
-      contradict H; trivial.
+    - case_eq a; intros Ha; case_eq b; intros Hb; simpl; trivial;
+      rewrite Ha, Hb in *; try contradict H; trivial.
  Qed.
 
   Lemma var_eq_refl : 
@@ -200,8 +193,6 @@ Module Lang <: Formulas.
 
 
 
-
-
   (* MLFormula *)
   Inductive MLFormula : Type :=
     | T : MLFormula
@@ -209,34 +200,149 @@ Module Lang <: Formulas.
     | NotML : MLFormula -> MLFormula
     | AndML : MLFormula -> MLFormula -> MLFormula
     | ExistsML : list Var -> MLFormula -> MLFormula
-    | enc : MLFormula -> MLFormula .
 
-  Eval compute in pattern (cfg (assign "a" (plus (id "a") (val 10))) nil).
-  Eval compute in pattern 
+    (* custom: add these by need; 
+       TODO: create separate type decls for basic ops and preds.
+    *)                                            
+    | gteML : Var -> Z -> MLFormula.
+
+  Notation "A >>= B" := (gteML A B) (at level 100).
+  
+  Eval compute in pattern
                     (cfg 
-                       (assign "a" (plus (id "a") (val 10))) 
-                       (("a" |-> (exp_var (exp_v "A"))) :: nil)).
+
+                     (i <- (val 0) ; (s <- (val 0)) ;
+                   (while (leq ($ i) ($ n))
+                          ((s <- (plus ($ s) ($ i))) ;
+                           (i <- (plus ($ i) (val 1))))
+                   ))
+                     
+                   ((n |-> (! N)) :: nil)).
+
   Eval compute in AndML T
-                        ( pattern 
-                            (cfg 
-                               (assign "a" (plus (id "a") (val 10))) 
-                               (("a" |-> (exp_var (exp_v "A"))) :: nil))).
+                     (pattern (cfg 
+
+                     (i <- (val 0) ; (s <- (val 0)) ;
+                   (while (leq ($ i) ($ n))
+                          ((s <- (plus ($ s) ($ i))) ;
+                           (i <- (plus ($ i) (val 1))))
+                   ))
+                     
+                   ((n |-> (! N)) :: nil))).
+
+  Eval compute in AndML
+                    (N >>= 0)
+                    (pattern (cfg 
+                        (i <- (val 0) ; (s <- (val 0)) ;
+                        (while (leq ($ i) ($ n))
+                           ((s <- (plus ($ s) ($ i))) ;
+                            (i <- (plus ($ i) (val 1))))
+                        ))
+                     ((n |-> (! N)) :: nil))).
+
+  
   (* Valuation *)
-  Definition Valuation (v : Var) : Model :=
+  Definition Valuation : Type := Var -> Model .
+
+
+  (* apply valuations *)
+  Fixpoint applyValExp (rho : Valuation) (e : Exp) : option _exp :=
+    match e with
+      | id j => Some (_id j)
+      | val v => Some (_int v)
+      | var_exp v => match (rho v) with
+                       | to_m_exp e => Some e
+                       | _ => None
+                     end
+      | plus v v' => match (applyValExp rho v), (applyValExp rho v') with
+                       | Some e', Some e'' => Some (_plus e' e'')
+                       | _, _ => None
+                     end
+    end.
+
+
+  Fixpoint applyValBExp (rho : Valuation) (b : BExp) : option _bexp :=
+    match b with
+      | TT => Some (_bool true)
+      | FF => Some (_bool false)
+      | var_bexp v => match (rho v) with
+                        | to_m_bexp b' => Some b'
+                        | _ => None
+                      end
+      | leq v v' => match (applyValExp rho v), (applyValExp rho v') with
+                       | Some e', Some e'' => Some (_leq e' e'')
+                       | _, _ => None
+                     end
+    end.
+
+
+  Fixpoint applyValStmt (rho: Valuation)(st : Stmt) : option _stmt :=
+    match st with
+      | assign x' e' => match (applyValExp rho e') with
+                          | Some e'' => Some (_assign x' e'')
+                          | _ => None
+                        end
+      | ifelse b' s1 s2 => match (applyValBExp rho b'), (applyValStmt rho s1), (applyValStmt rho s2) with
+                             | Some b'', Some s1', Some s2' => Some (_ifelse b'' s1' s2')
+                             | _, _, _ => None
+                           end
+      | while b' s' => match (applyValBExp rho b'), (applyValStmt rho s') with
+                         | Some b'', Some s'' => Some (_while b'' s'')
+                         | _, _ => None
+                       end
+      | seq s1 s2 => match (applyValStmt rho s1), (applyValStmt rho s2) with
+                       | Some s1', Some s2' => Some (_seq s1' s2')
+                       | _, _ => None
+                     end
+      | skip => Some _skip
+      | var_stmt s' => match (rho s') with
+                         | to_m_stmt s' => Some s'
+                         | _ => None
+                       end
+    end.
   
 
-Print Exp.
-Print ExpVar.
-Print Var.
-Print Model.
-Print _exp.
-  (* Sat ML *)
-  Fixpoint applyValExpVar (rho : Valuation) (v : ExpVar) : _nat := 
-  match v with 
-  | exp_v v' => 
+  Fixpoint applyValMapItem (rho : Valuation) (mi : MapItem) : option _map_item :=
+    match mi with
+      | var_mi v => match (rho v) with
+                      | to_m_map_item m => Some m
+                      | _ => None
+                    end
+      | item i' v => match (applyValExp rho v) with
+                       | Some e' => Some  (i, e')
+                       | _ => None
+                     end
+    end.
 
-  Fixpoint applyValExp (rho : Valuation) (e : Exp) : _exp :=
-    match e with
-    | exp_var v => applyValExpVar rho v
+
+  Print Model.
+  Print Cfg .
+  Print MapItem.
+
+  Fixpoint applyValList (rho: Valuation) (l : list MapItem) : option (list _map_item) :=
+    match l with
+      | nil => None
+      | e :: l' => match (applyValMapItem rho e), (applyValList rho l') with
+                     | Some v', Some l'' =>  Some (v' :: l'')
+                     | _, _ => None
+                   end
+    end.
+      
+  
+  Fixpoint applyVal (rho : Valuation) (c : Cfg) : option State := 
+    match c with
+      | var_cfg v => match (rho v) with
+                       | to_m_state s' => Some s'
+                       | _ => None
+                     end
+      | cfg st l => match (applyValStmt rho st), (applyValList rho l) with
+                     | Some s',  Some l' => Some (s', l')
+                     | _, _ => None
+                   end
+    end.
+
+
+  
+  
 
 End Lang.
